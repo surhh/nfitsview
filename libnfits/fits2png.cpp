6e44cded0e6d75b169356fd3cd5a64bf2614a662
surhh <109148999+surhh@users.noreply.github.com>
2022-11-22 16:32:00 +0400

    Minor cleanup of code

--- libnfits/fits2png.cpp
+++ libnfits/fits2png.cpp
@@ -4,14 +4,12 @@
 namespace libnfits
 {
 
+//// This function will be needed in the future, to support standalone conversions via libnfits
 int32_t convertFITS2PNG(const std::string& a_fitsFileName, const std::string& a_pngFileName)
 {
     int32_t retVal = FITS_GENERAL_SUCCESS;
 
     std::string fitsFileName = a_fitsFileName, pngFileName = a_pngFileName;
-
-    //// TODO: This function will be needed in the future, to support command-line interface.
-    ////       Will be implemented when converting FITS to PNG via command line arguments.
 
     return retVal;
 }
